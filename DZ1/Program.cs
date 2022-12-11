eea5cd943ff57c26efcf9f3381efdbefbe9e47e8
Anton <Scccq@yandex.ru>
2022-12-11 17:07:20 +0300

    Добавили DZ1

---
+++ DZ1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Семинар 4, Домашняя задача 25: 
+//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Write("Введите число A= ");
+int numA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B= ");
+int numB = int.Parse(Console.ReadLine()!);
+
+
+int expFunction = Power(numA, numB);
+Console.WriteLine(expFunction);
+
+
+int Power(int numberA, int numberB)
+{
+    int expAinB = numberA;
+    for (int i = 1; i < numberB; i++)
+    {
+        expAinB = expAinB * numberA;
+    }
+    return expAinB;
+}
